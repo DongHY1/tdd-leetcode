27520ed56a72b49a6401232ca0b41ca6a8125c79
haoyudong <2070343054@email.szu.edu.cn>
2022-09-22 22:15:29 +0800

    happy path

---
+++ src/test.test.js
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+import { foo } from './test';
+describe('happy path', () => {
+  it('return a+b', () => {
+    expect(foo(1, 2)).toEqual(3);
+  });
+});
