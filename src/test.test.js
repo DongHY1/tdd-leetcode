5b70f13ddadf200bcd1d3458fc3171736ba2dd10
haoyudong <2070343054@email.szu.edu.cn>
2022-09-25 10:55:17 +0800

    use vitest debug

--- src/test.test.js
+++ src/test.test.js
@@ -2,7 +2,6 @@ import { describe, it, expect } from 'vitest';
 import { foo } from './test';
 describe('happy path', () => {
   it('return a+b', () => {
-    console.log(1);
     expect(foo(1, 2)).toEqual(3);
   });
 });
