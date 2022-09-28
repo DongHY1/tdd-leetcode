4a8da081101db57a4d54b43264ef717aaaa0a31f
haoyudong <2070343054@email.szu.edu.cn>
2022-09-28 12:28:01 +0800

    compelete 95

--- src/95. Unique Binary Search Trees II.ts
+++ src/95. Unique Binary Search Trees II.ts
@@ -1,4 +1,21 @@
 import { TreeNode } from './common/TreeNode';
 export function generateTrees(n: number): Array<TreeNode | null> {
-  return [null];
+  const dfs = (start: number, end: number): Array<TreeNode | null> => {
+    let res: Array<TreeNode | null> = [];
+    if (start > end) return [null];
+    for (let i = start; i <= end; i++) {
+      const leftNode = dfs(start, i - 1);
+      const rightNode = dfs(i + 1, end);
+      for (const l of leftNode) {
+        for (const r of rightNode) {
+          const root = new TreeNode(i);
+          root.left = l;
+          root.right = r;
+          res.push(root);
+        }
+      }
+    }
+    return res;
+  };
+  return dfs(1, n);
 }
--- src/__test__/95.test.ts
+++ src/__test__/95.test.ts
@@ -1,8 +1,20 @@
 import { generateTrees } from '../95. Unique Binary Search Trees II';
 import { TreeNode } from '../common/TreeNode';
 import { describe, it, expect } from 'vitest';
-describe('tree', () => {
-  it('pass zero ', () => {
+describe('95', () => {
+  it('pass zero return null ', () => {
     expect(generateTrees(0)).toEqual([null]);
+  });
+  it('pass one return one tree', () => {
+    const node = new TreeNode(1);
+    expect(generateTrees(1)).toEqual([node]);
+  });
+  it('pass 3 return the first element equal to res', () => {
+    const res = new TreeNode(
+      1,
+      null,
+      new TreeNode(2, null, new TreeNode(3, null, null))
+    );
+    expect(generateTrees(3)[0]).toEqual(res);
   });
 });
