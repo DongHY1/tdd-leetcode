c11a0fd9049162a170ec9b746840fdec98a9cfbf
haoyudong <2070343054@email.szu.edu.cn>
2022-09-30 13:52:38 +0800

    compelete 103

---
+++ src/103. Binary Tree Zigzag Level Order Traversal.ts
@@ -0,0 +1,25 @@
+import { TreeNode } from './common/TreeNode';
+export function zigzagLevelOrder(root: TreeNode | null): number[][] {
+  let res: number[][] = [];
+  let flag = true;
+  if (root === null) return res;
+  let queue: TreeNode[] = [];
+  queue.push(root);
+  while (queue.length) {
+    let level: number[] = [];
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
+      let node: TreeNode = queue.shift()!;
+      if (flag) {
+        level.push(node.val);
+      } else {
+        level.unshift(node.val);
+      }
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    res.push(level);
+    flag = !flag;
+  }
+  return res;
+}
---
+++ src/__test__/103.test.ts
@@ -0,0 +1,40 @@
+import { describe, it, expect } from 'vitest';
+import { TreeNode } from '../common/TreeNode';
+import { zigzagLevelOrder } from '../103. Binary Tree Zigzag Level Order Traversal';
+describe('', () => {
+  it('null', () => {
+    expect(zigzagLevelOrder(null)).toEqual([]);
+  });
+  it('one element', () => {
+    const root = new TreeNode(5, null, null);
+    expect(zigzagLevelOrder(root)).toEqual([[5]]);
+  });
+  it('one level', () => {
+    const root = new TreeNode(
+      5,
+      new TreeNode(1, null, null),
+      new TreeNode(6, null, null)
+    );
+    expect(zigzagLevelOrder(root)).toEqual([[5], [6, 1]]);
+  });
+  it('two level', () => {
+    const root = new TreeNode(
+      3,
+      new TreeNode(9, null, null),
+      new TreeNode(
+        20,
+        new TreeNode(15, null, null),
+        new TreeNode(7, null, null)
+      )
+    );
+    expect(zigzagLevelOrder(root)).toEqual([[3], [20, 9], [15, 7]]);
+  });
+  it('two level ', () => {
+    const root = new TreeNode(
+      1,
+      new TreeNode(2, new TreeNode(4, null, null), new TreeNode(5, null, null)),
+      new TreeNode(3, null, null)
+    );
+    expect(zigzagLevelOrder(root)).toEqual([[1], [3, 2], [4, 5]]);
+  });
+});
