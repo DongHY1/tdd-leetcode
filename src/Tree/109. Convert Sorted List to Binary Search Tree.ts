7eebad9d25e3f54ef78f1e91e76c7dbdf6dbb434
haoyudong <2070343054@email.szu.edu.cn>
2022-10-02 16:07:23 +0800

    compelete 109 use listnode

--- src/Tree/109. Convert Sorted List to Binary Search Tree.ts
+++ src/Tree/109. Convert Sorted List to Binary Search Tree.ts
@@ -21,3 +21,24 @@ function transfromListToArray(head: ListNode | null): number[] {
   }
   return res;
 }
+
+// 方法二：找到链表的中点 利用快慢指针找到链表的中点，然后把链表分成两部分，递归的构建二叉搜索树
+export function sortedListToBST_1(head: ListNode | null): TreeNode | null {
+  if (head === null) return null;
+  // 把链表转换为数组
+  const helper = (l:ListNode,r:ListNode|null) => {
+    if (l ===r!) return null;
+    const mid = findMidListNode(l,r!);
+    return new TreeNode(mid.val, helper(l, mid), helper(mid.next!, r));
+  };
+  return helper(head,null);
+}
+function findMidListNode(l:ListNode,r:ListNode): ListNode {
+  let slow = l;
+  let fast = l;
+  while (fast!==r && fast.next!==r) {
+    slow = slow.next!;
+    fast = fast.next!.next!;
+  }
+  return slow;
+}
--- src/__test__/Tree/109.test.ts
+++ src/__test__/Tree/109.test.ts
@@ -1,9 +1,13 @@
 import { describe, it, expect } from 'vitest';
 import { TreeNode, ListNode } from '../../common';
-import { sortedListToBST } from '../../Tree/109. Convert Sorted List to Binary Search Tree';
+import {
+  sortedListToBST,
+  sortedListToBST_1,
+} from '../../Tree/109. Convert Sorted List to Binary Search Tree';
 describe('', () => {
   it('give a empty input', () => {
     expect(sortedListToBST(null)).toEqual(null);
+    expect(sortedListToBST_1(null)).toEqual(null);
   });
   it('give a node input', () => {
     const node = new ListNode(
@@ -15,6 +19,13 @@ describe('', () => {
         0,
         new TreeNode(-10, null, new TreeNode(-3, null, null)),
         new TreeNode(5, null, new TreeNode(9, null, null))
+      )
+    );
+    expect(sortedListToBST_1(node)).toEqual(
+      new TreeNode(
+        0,
+        new TreeNode(-3,new TreeNode(-10, null, null),null),
+        new TreeNode(9,new TreeNode(5, null, null),null)
       )
     );
   });
