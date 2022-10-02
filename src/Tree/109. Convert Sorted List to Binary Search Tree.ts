48337630f13654a6bfcd3ea1a10244a1bc0bd51d
haoyudong <2070343054@email.szu.edu.cn>
2022-10-02 12:32:29 +0800

    compelete 109

--- src/Tree/109. Convert Sorted List to Binary Search Tree.ts
+++ src/Tree/109. Convert Sorted List to Binary Search Tree.ts
@@ -1,6 +1,23 @@
 import { ListNode, TreeNode } from '../common';
 export function sortedListToBST(head: ListNode | null): TreeNode | null {
   if (head === null) return null;
-  
-  return null;
+  // 把链表转换为数组
+  const arr = transfromListToArray(head);
+  const helper = (nums: number[], l: number, r: number) => {
+    if (l > r) return null;
+    const mid = Math.floor((l + r) / 2);
+    const root = new TreeNode(nums[mid]);
+    root.left = helper(nums, l, mid - 1);
+    root.right = helper(nums, mid + 1, r);
+    return root;
+  };
+  return helper(arr, 0, arr.length - 1);
+}
+function transfromListToArray(head: ListNode | null): number[] {
+  const res: number[] = [];
+  while (head !== null) {
+    res.push(head.val);
+    head = head.next;
+  }
+  return res;
 }
--- src/__test__/Tree/109.test.ts
+++ src/__test__/Tree/109.test.ts
@@ -5,4 +5,17 @@ describe('', () => {
   it('give a empty input', () => {
     expect(sortedListToBST(null)).toEqual(null);
   });
+  it('give a node input', () => {
+    const node = new ListNode(
+      -10,
+      new ListNode(-3, new ListNode(0, new ListNode(5, new ListNode(9))))
+    );
+    expect(sortedListToBST(node)).toEqual(
+      new TreeNode(
+        0,
+        new TreeNode(-10, null, new TreeNode(-3, null, null)),
+        new TreeNode(5, null, new TreeNode(9, null, null))
+      )
+    );
+  });
 });
